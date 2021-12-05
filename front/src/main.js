43e90d192ce4ca5a4628d4e965c763a43c708db7
chum-ch <yoeurnchum@gmail.com>
2021-12-05 14:52:42 +0700

    #6 updte

--- front/src/main.js
+++ front/src/main.js
@@ -2,15 +2,12 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue'
 import login from './components/LoginForm/login.vue'
-import signup from './components/SignUp/signup.vue';
 const app = createApp(App);
 app.component('login', login);
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {path: '/', component:login},
-    {path: '/signup', component:signup}
   ],
 });
 
