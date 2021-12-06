2231f2b5baedea65e9e3d115980afdf680350a16
engsophorn <sophorn.eng@student.passerellesnumeriques.org>
2021-12-06 08:21:31 +0700

    #20 categories

--- front/src/main.js
+++ front/src/main.js
@@ -1,45 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue'
-
-/* =================|-IMPORT USER PAGE-|=================*/
-import Login from './components/LoginForm/Login.vue' 
-
-
-import SignUp from './components/SignUp/SignUp.vue'
-
-
-import MenuBar from './components/MenuBar/MenuBar.vue'
-
-
-import Event from './components/Event-Page/My-Event-Page.vue'
-
-import AllEvent from './components/Event-Page/All-Event.vue'
-
-import CategoriesView from './components/Categories/CategoriesView.vue'
-
-
-
-
+import router from './router' 
 /* =-=-=-=-=-=-=-=-=-=-=-=|--|=-=-=-=-=-=-=-=-=-=-=-= */
 const app = createApp(App)
-app.component('signin', Login)
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: Login },
-    { path: '/signup', component: SignUp},
-    { path: '/menu-bar', component: MenuBar},
-    { path: '/events', component: Event},
-    { path: '/all-events', component: AllEvent},
-    { path: '/categories', component: CategoriesView},
-    
-  ],
-});
-  
 app.use(router)
 app.mount('#app')
-
-
 
